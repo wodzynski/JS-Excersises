f82b6a2f81ac83fc8606165eb5353583c073cc8a
wodzynski <macko5395@gmail.com>
2018-06-10 14:52:56 +0200

    jQuery basic effects

---
+++ jQuery/Effects/main.js
@@ -0,0 +1,27 @@
+$('#fade').on('click', function() {
+    $('div').fadeOut(1000, function() {
+        // $(this).remove();
+        console.log("Fade Completed!");
+    });
+});
+
+$('#fadeback').on('click', function() {
+    $('div').fadeIn(1000, function() {
+        console.log("Fade Completed!");
+    });
+});
+
+$('#slidedown').on('click', function() {
+    $('div').slideDown(100);
+});
+
+$('#slideup').on('click', function() {
+    $('div').slideUp(100);
+});
+
+$('#slidetoggle').on('click', function() {
+    $('div').slideToggle(1000, function() {
+        console.log("SLIDE IS DONE!");
+        // $(this).remove();
+    });
+});
